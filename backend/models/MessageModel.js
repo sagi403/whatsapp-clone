90f4abdb69c74ee78c7c369755f81fad478e254b
Sagi <sagi403@gmail.com>
2023-02-09 15:14:25 +0200

    Adding message model

---
+++ backend/models/MessageModel.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    conversationId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "Conversation",
+    },
+    sender: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
