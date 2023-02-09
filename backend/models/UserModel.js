741960db46ab0dd5083782038ee0b4424ebd866f
Sagi <sagi403@gmail.com>
2023-02-09 16:11:53 +0200

    Updating the user model

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -6,19 +6,29 @@ const userSchema = new mongoose.Schema(
     username: {
       type: String,
       required: true,
+      min: 3,
+      max: 20,
+      unique: true,
     },
     email: {
       type: String,
       required: true,
+      max: 50,
+      unique: true,
     },
     password: {
       type: String,
       required: true,
+      min: 6,
     },
     isAdmin: {
       type: Boolean,
       required: true,
       default: false,
+    },
+    avatarColors: {
+      type: Array,
+      required: true,
     },
   },
   {
