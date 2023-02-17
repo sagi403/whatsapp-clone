984b2cf940379afda03eca3c6f6eda3748636680
Sagi <sagi403@gmail.com>
2023-02-17 15:00:27 +0200

    Adding add conversation button to the sidebar

--- frontend/src/components/base/Sidebar.js
+++ frontend/src/components/base/Sidebar.js
@@ -25,20 +25,25 @@ const Sidebar = ({ currentDialog, setCurrentDialog, setArrivalMessages }) => {
   };
 
   return (
-    <div className="lg:w-1/4 md:w-1/4 w-1/2 bg-gray-200 h-screen">
-      <ul className="list-none p-0">
-        {conversations?.map(dialog => (
-          <Dialog
-            name={dialog.name}
-            lastMessage={dialog.lastMessage}
-            receivedAt={dialog.receivedAt}
-            avatar={dialog.avatar}
-            key={dialog.userId}
-            active={currentDialog?.userId === dialog.userId}
-            onClick={() => handleDialogClick(dialog)}
-          />
-        ))}
-      </ul>
+    <div className="lg:w-1/4 md:w-1/4 w-1/2 bg-gray-200 h-screen relative">
+      <div className="container overflow-auto h-full pb-16">
+        <ul className="list-none p-0">
+          {conversations?.map(dialog => (
+            <Dialog
+              name={dialog.name}
+              lastMessage={dialog.lastMessage}
+              receivedAt={dialog.receivedAt}
+              avatar={dialog.avatar}
+              key={dialog.userId}
+              active={currentDialog?.userId === dialog.userId}
+              onClick={() => handleDialogClick(dialog)}
+            />
+          ))}
+        </ul>
+      </div>
+      <button className="bg-green-600 hover:bg-green-700 text-white py-3 px-4 w-full absolute bottom-0 left-0 right-0">
+        Add Conversation
+      </button>
     </div>
   );
 };
