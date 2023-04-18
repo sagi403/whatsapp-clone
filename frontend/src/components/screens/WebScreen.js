eface42bc996df06416b9e94fff8001367c60f3c
Sagi <sagi403@gmail.com>
2023-04-18 16:04:07 +0300

    Extracting the send options logic to other component

--- frontend/src/components/screens/WebScreen.js
+++ frontend/src/components/screens/WebScreen.js
@@ -1,8 +1,7 @@
 import { ethers } from "ethers";
 import { toWei } from "ethers/lib/utils";
-import { Menu, Transition } from "@headlessui/react";
-import { useEffect, useRef, useState, Fragment } from "react";
-import { PaperAirplaneIcon, BoltIcon } from "@heroicons/react/20/solid";
+import { useEffect, useRef, useState } from "react";
+import { PaperAirplaneIcon } from "@heroicons/react/20/solid";
 import UserHeader from "../base/UserHeader";
 import { useAuth } from "../../hooks/useAuth";
 import { useSocket } from "../../hooks/useSocket";
@@ -13,8 +12,7 @@ import { addNewUnreadMessage } from "../../fetchers/addNewUnreadMessage";
 import UnreadMessage from "../base/UnreadMessage";
 import useMessages from "../../hooks/useMessages";
 import useUnreadMessages from "../../hooks/useUnreadMessages";
-
-const ETHER_SEND_AMOUNT = [5, 20, 50, 100];
+import SendOptions from "../base/SendOptions";
 
 const WebScreen = () => {
   const [message, setMessage] = useState("");
@@ -187,45 +185,7 @@ const WebScreen = () => {
               </div>
               {/* Input message */}
               <div className="flex p-3 bg-gray-200">
-                <div className="relative inline-block text-left">
-                  <Menu as="div" className="relative inline-block text-left">
-                    <div>
-                      <Menu.Button className="py-2 px-2 inline-flex items-center justify-center text-gray-500 hover:text-gray-600 focus:outline-none">
-                        <BoltIcon
-                          className="h-7 w-7 text-yellow-500"
-                          aria-hidden="true"
-                        />
-                      </Menu.Button>
-                    </div>
-                    <Transition
-                      as={Fragment}
-                      enter="transition ease-out duration-100"
-                      enterFrom="transform opacity-0 scale-95"
-                      enterTo="transform opacity-100 scale-100"
-                      leave="transition ease-in duration-75"
-                      leaveFrom="transform opacity-100 scale-100"
-                      leaveTo="transform opacity-0 scale-95"
-                    >
-                      <Menu.Items className="absolute left-0 top-0 mt-[-48px] rounded-md shadow-lg bg-opacity-20 bg-gray-500">
-                        <div className="flex justify-around py-1" role="none">
-                          {ETHER_SEND_AMOUNT.map((amount, index) => (
-                            <Menu.Item key={index}>
-                              <button
-                                className="text-gray-700 mx-1 bg-white border-2 border-yellow-400 rounded-full w-10 h-10 flex items-center justify-center hover:bg-yellow-100 focus:outline-none"
-                                role="menuitem"
-                                tabIndex="-1"
-                                onClick={() => handleSendEther(amount)}
-                              >
-                                {amount}
-                              </button>
-                            </Menu.Item>
-                          ))}
-                        </div>
-                      </Menu.Items>
-                    </Transition>
-                  </Menu>
-                </div>
-
+                <SendOptions handleSendEther={handleSendEther} />
                 <input
                   type="text"
                   className="p-2 w-full rounded-lg focus:outline-none shadow-input shadow-gray-300"
---
+++ frontend/src/constants/consts.js
@@ -0,0 +1,1 @@
+export const ETHER_SEND_AMOUNT = [5, 20, 50, 100];
